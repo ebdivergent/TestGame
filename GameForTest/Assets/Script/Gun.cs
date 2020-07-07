2d5880cf54b93369d537c847b0a3c51facd372ff
ebdivergent <ebdivergent@gmail.com>
2020-07-07 17:33:29 +0300

    Shot (touch check)

--- GameForTest/Assets/Script/Gun.cs
+++ GameForTest/Assets/Script/Gun.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform bullet;
+    
+    public float damage = 10f;
+    public int BulletForce = 1000;
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        if (Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            Transform BulletInstance = (Transform)Instantiate(bullet, GameObject.Find("FirePoint").transform.position, Quaternion.identity);
+            BulletInstance.GetComponent<Rigidbody>().AddForce(transform.forward * BulletForce);
+            Debug.Log("Piu");
+        }
     }
 }
